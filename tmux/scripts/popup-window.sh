0d20449f17ac30a39c055ed7f1483b6cbd36d732
reynoldputra <reynoldputra1@gmail.com>
2024-02-19 21:24:12 +0700

    fix: update tmux

--- tmux/scripts/popup-window.sh
+++ tmux/scripts/popup-window.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
 POPUP_SESSION_NAME="scratch"
+DIR="${1:-}"
 
 if [ "$(tmux display-message -p -F "#{session_name}")" = "$POPUP_SESSION_NAME" ];then
     tmux detach-client
+    exit 0
+fi
+
+if [ -z "$DIR" ]; then
+    tmux popup -E -h '80%' -w '80%' "tmux attach -t $POPUP_SESSION_NAME || tmux new -s $POPUP_SESSION_NAME"
 else
     tmux popup -E -h '80%' -w '80%' "tmux attach -t $POPUP_SESSION_NAME || tmux new -s $POPUP_SESSION_NAME"
 fi
